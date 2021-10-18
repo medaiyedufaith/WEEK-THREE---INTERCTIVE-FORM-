e985c95a981933d0adfa4f59d15da1f034b8a9e5
medaiyedufaith <medaiyedufaith@gmail.com>
2021-10-18 08:30:32 -0700

    INTERACTIVE FORM

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 function decrBtn() {
-    var value = parseInt(document.getElementsByClassName('disp-people').value, 10);
-    value = isNaN(value) ? 0: value;
-    value++
-    document.getElementsByClassName('disp-people').value = value + " " + "people"
+    var count = parseInt(document.getElementsByClassName('disp-people').value, 10);
+    count = isNaN(count) ? 0: count;
+    count--
+    console.log(document.getElementsByClassName('disp-people'))
+    document.getElementsByClassName('disp-people').value = count + " " + "people"
 }
 
 function incrBtn() {
     var value = parseInt(document.getElementsByClassName('disp-people').value, 10);
     value = isNaN(value) ? 0: value;
-    value--
+    value++
     document.getElementsByClassName('disp-people').value = value + " " + "people"
 }
 function submitBtn() {
-    swal("Good job!", "Your Space have been reserved", "success");
+    swal("Good job!", "Your Space has been reserved", "success");
 }
